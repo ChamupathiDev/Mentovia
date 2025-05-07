a06c6c93b706c209509877cab416d735a73a6383
Dineth Chamupathi Kumara <127776710+ChamupathiDev@users.noreply.github.com>
2025-05-07 09:19:28 +0530

    Delete backend directory

--- backend/src/main/java/com/mentovia/repository/LearningResourceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mentovia.repository;
-
-import com.mentovia.model.LearningResource;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LearningResourceRepository extends MongoRepository<LearningResource,String> { }
-
-
